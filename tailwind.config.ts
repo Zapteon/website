84dcb4dc236a213e8757d8863d24d387f023b173
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-04-12 12:44:26 +0200

    style(tailwind): add default colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#141516",
+          dark: "#151515",
+        },
+        secondary: {
+          DEFAULT: "#ffffff",
+          dark: "#c0c0c0",
+        },
+        additional: {
+          DEFAULT: "#00F4A2",
+          dark: "#02AE74",
+        },
+      },
       spacing: {
         box: "1180px",
         boxTablet: "600px",
