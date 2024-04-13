6c6317d2ecb0c8377b5c4d82ca93d32a3849d637
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-04-13 14:51:09 +0200

    feat(navbar): add mobile navigation component

--- src/components/pages/additional/navbar/Navbar.tsx
+++ src/components/pages/additional/navbar/Navbar.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/components/ui/button";
+import { MobileNavigation } from "./MobileNavigation";
 
 const navLinks: string[] = ["Pricing", "Enterprise", "About"]
 
 const Navigation = () => {
   return (
     <div className="flex flex-row items-center justify-center">
-      <Link href="/" className="flex flex-row items-center justify-center">
+      <Link href="/" className="flex flex-row items-center justify-center ml-3">
         <Image src="logo.svg" width={40} height={40} alt="logo" priority={true} className="mr-1" />
         <div className="h-6 text-4xl font-semibold flex items-center justify-center mb-3 hover:text-secondary-dark transition-colors duration-300 ease-in-out">
           zapteon
@@ -46,8 +47,9 @@ const AuthSection = () => {
 export default function Navbar() {
   return (
     <div className="flex justify-center w-screen py-10">
-      <div className="xs:w-boxPhone lg:w-boxTablet xl:w-box font-bold flex items-center justify-between">
+      <div className="w-boxPhone sm:w-boxTablet xl:w-box font-bold flex items-center justify-between">
         <Navigation />
+        <MobileNavigation />
         <AuthSection />
       </div>
     </div>
