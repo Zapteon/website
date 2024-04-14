5c5a8be69b98fc56916615317d4c207beb244cd4
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-04-14 10:39:01 +0200

    feat(main): implement hero section

---
+++ src/components/pages/main/Hero.tsx
@@ -0,0 +1,11 @@
+"use client"
+import Spline from '@splinetool/react-spline';
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className="flex justify-center items-center">
+      <Spline scene="https://draft.spline.design/ZrdjB5aSBQHbGeFh/scene.splinecode" />
+    </div>
+  );
+}
