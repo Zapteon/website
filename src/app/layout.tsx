1378c8ddbe5045667e87ff1d9261a022076bf588
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-04-14 10:38:43 +0200

    build: install spline

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Jost } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import Navbar from "@/components/pages/additional/navbar/Navbar";
+import Navbar from "@/components/additional/navbar/Navbar";
 
 const jost = Jost({ subsets: ["latin"] });
 
