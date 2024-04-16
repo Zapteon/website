145a8470faf73284370be424735d4a4948dcaf40
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-04-16 11:04:30 +0200

    feat(hero): add media queries for animation

--- src/components/pages/main/SplineHero.tsx
+++ src/components/pages/main/SplineHero.tsx
@@ -1,8 +1,33 @@
 "use client"
+
+import React, { useState, useEffect } from 'react';
 import Spline from "@splinetool/react-spline";
 
 export default function Hero() {
+  const [displayStyle, setDisplayStyle] = useState('none');
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 1280) {
+        setDisplayStyle('block');
+      } else {
+        setDisplayStyle('none');
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <Spline scene="https://draft.spline.design/P1q6cAQr0QnVGV5n/scene.splinecode" />
+    <div style={{ display: displayStyle }}>
+      <Spline scene="https://draft.spline.design/kyaCdirl0VCeX6eU/scene.splinecode" style={{ marginRight: '40px', width: '700px', height: '880px' }} />
+    </div>
   );
 }
+
